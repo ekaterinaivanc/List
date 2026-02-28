3522a0b6b7ac4a5a5b39c46ddecf64061304c7e8
ekaterinaivanc <kataivancova7@gmail.com>
2026-02-28 11:25:45 +0300

    add create

--- main.cpp
+++ main.cpp
@@ -4,3 +4,12 @@ struct BiList {
     BiList<T> * next;
     BiList<T> * prev;
 };
+
+template< class T>
+BiList<T> * create(const T& val)
+{
+    BiList<T> * node = new BiList<T>{val, nullptr, nullptr};
+    node->next = node;
+    node->prev = node;
+    return node;
+}
